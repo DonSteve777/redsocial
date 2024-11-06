42382e6ed3520e725d8dba10995da824ccf6d4cd
DonSteve777 <93adones@gmail.com>
2024-11-06 15:57:08 +0100

    get users jpa

---
+++ src/main/java/redsocial/user/UserService.java
@@ -0,0 +1,26 @@
+package redsocial.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return (List<User>)userRepository.findAll();
+    }   
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+}
